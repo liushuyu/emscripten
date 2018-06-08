6c4936e1b2f033f687882809fe1d98289381a7e3
liushuyu <liushuyu011@gmail.com>
2018-06-08 00:23:56 -0600

    signals: add ualarm, get/setitimer

--- src/library_signals.js
+++ src/library_signals.js
@@ -1,10 +1,11 @@
 // 'use strict'
 var funs = {
   _sigalrm_handler: 0,
+  _itimers: {},
 
   signal__deps: ['_sigalrm_handler'],
   signal: function(sig, func) {
-    if (sig == 14 /*SIGALRM*/) {
+    if (sig == 14 /*SIGALRM*/ && func !== 1) {
       __sigalrm_handler = func;
     } else {
 #if ASSERTIONS
@@ -103,14 +104,90 @@ var funs = {
       if (__sigalrm_handler) Module['dynCall_vi'](__sigalrm_handler, 0);
     }, seconds*1000);
   },
-  ualarm: function() {
-    throw 'ualarm() is not implemented yet';
+
+  // http://pubs.opengroup.org/onlinepubs/009695399/functions/ualarm.html
+  ualarm__deps: ['_sigalrm_handler'],
+  ualarm: function(timeOut, intervals) {
+    if (timeOut < 0 || intervals < 0) {
+       ___setErrNo(ERRNO_CODES.EINVAL);
+       return -1;
+    }
+    setTimeout(function() {
+      if (__sigalrm_handler) Runtime.dynCall('vi', __sigalrm_handler, [0]);
+      }, timeOut / 1000);
+    setInterval(function() {
+      if (__sigalrm_handler) Runtime.dynCall('vi', __sigalrm_handler, [0]);
+    }, intervals / 1000);
+    return 0;
   },
-  setitimer: function() {
-    throw 'setitimer() is not implemented yet';
+
+  // http://pubs.opengroup.org/onlinepubs/9699919799/functions/getitimer.html
+  // On the same page as `getitimer`
+  // struct of thisTimer object:
+  // {shot: [single shot timeout], shotTimer: [returned by setTimeout],
+  // interval: [interval timeout], intervalTimer: [returned by setInterval]}
+  setitimer__deps: ['_sigalrm_handler'],
+  setitimer: function(which, new_value, old_value) {
+    if (which == 0 || which == 1 || which == 2) {
+      var thisTimer = __itimers[which];
+      if (thisTimer) {
+        getitimer(which, old_value);
+      }
+      thisTimer = {};
+      if (new_value) {
+        var oneshot_sec = {{{makeGetValue('new_value', C_STRUCTS.itimerspec.it_value.tv_sec, 'i16')}}};
+        var oneshot_usec = {{{makeGetValue('new_value', C_STRUCTS.itimerspec.it_value.tv_nsec, 'i16')}}};
+        var interval_sec = {{{makeGetValue('new_value', C_STRUCTS.itimerspec.it_interval.tv_sec, 'i16')}}};
+        var interval_usec = {{{makeGetValue('new_value', C_STRUCTS.itimerspec.it_interval.tv_nsec, 'i16')}}};
+        if (thisTimer) {
+          clearInterval(thisTimer.intervalTimer);
+          clearTimeout(thisTimer.shotTimer);
+        }
+        if (oneshot_sec < 0 || oneshot_usec < 0 || oneshot_usec > 999999 || interval_sec < 0 || interval_usec < 0 || interval_usec > 999999) {
+          ___setErrNo(ERRNO_CODES.EINVAL);
+          return -1;
+        }
+        if (oneshot_sec !== null || oneshot_usec !== null) {
+          thisTimer.shot = {tv_sec: oneshot_sec, tv_usec: oneshot_usec};
+          thisTimer.shotTimer = setTimeout(function() {
+            if (__sigalrm_handler) Runtime.dynCall('vi', __sigalrm_handler, [0]);
+            thisTimer.shot = thisTimer.interval;
+          }, oneshot_sec * 1000 + oneshot_usec / 1000);
+        }
+        if (interval_sec !== null || interval_usec !== null) {
+          thisTimer.interval = {tv_sec: interval_sec, tv_usec: interval_usec};
+          thisTimer.intervalTimer = setInterval(function() {
+            if (__sigalrm_handler) Runtime.dynCall('vi', __sigalrm_handler, [0]);
+          }, interval_sec * 1000 + interval_usec / 1000);
+        }
+        __itimers[which] = thisTimer;
+      }
+      return 0;
+    }
+    ___setErrNo(ERRNO_CODES.EINVAL);
+    return -1;
   },
-  getitimer: function() {
-    throw 'getitimer() is not implemented yet';
+
+  // http://pubs.opengroup.org/onlinepubs/9699919799/functions/getitimer.html
+  getitimer__deps: ['_sigalrm_handler'],
+  getitimer: function(which, curr_value) {
+    if (which == 0 || which == 1 || which == 2) {
+      var thisTimer = __itimers[which];
+      if (thisTimer && curr_value) {
+        {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_value.tv_sec, 'thisTimer.shot.tv_sec', 'i16') }}}
+        {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_value.tv_nsec, 'thisTimer.shot.tv_usec', 'i16') }}}
+        {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_interval.tv_sec, 'thisTimer.interval.tv_sec', 'i16') }}}
+        {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_interval.tv_nsec, 'thisTimer.interval.tv_usec', 'i16') }}}
+        return 0;
+      }
+      {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_value.tv_sec, '0', 'i16') }}}
+      {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_value.tv_nsec, '0', 'i16') }}}
+      {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_interval.tv_sec, '0', 'i16') }}}
+      {{{ makeSetValue('curr_value', C_STRUCTS.itimerspec.it_interval.tv_nsec, '0', 'i16') }}}
+      return 0;
+    }
+    ___setErrNo(ERRNO_CODES.EINVAL);
+    return -1;
   },
 
   pause__deps: ['__setErrNo', '$ERRNO_CODES'],
@@ -175,4 +252,5 @@ var funs = {
   //sigset
 };
 
+autoAddDeps(funs, '_itimers');
 mergeInto(LibraryManager.library, funs);
